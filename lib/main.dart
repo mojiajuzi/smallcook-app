4e738b2d6840b0ec2c5f6ace17d1aa93de5af8ae
gru <1065407867@qq.com>
2018-04-19 23:39:16 +0800

    homepage set

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'menu_page.dart';
 
@@ -5,18 +8,38 @@ import 'menu_page.dart';
 void main()=>runApp(new HomePage());
 
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => new _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  List data;
 
   @override
+  void initState(){
+    super.initState();
+    getData();
+  }
+
+  getData()async{
+    var res = await getCategory();
+    if(!mounted) return;
+    setState(() {
+          data = res;
+        });
+  }
+
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'homepage',
       home: new Scaffold(
-        appBar: new AppBar(title: new Text('Card'),),
+        appBar: new AppBar(title: new Text('Your Menu'),),
         body: new Container(
           child: new Column(
-           children: <Widget>[
-             getListCategory(context)
+            children: <Widget>[
+             getListCategory(context, data)
            ],
           )
         ),
@@ -25,25 +48,25 @@ class HomePage extends StatelessWidget {
   }
 }
 
-Widget getListCategory(BuildContext context){
+Widget getListCategory(BuildContext context, List categoryList){
 
-  List<FoodCategory> categoryList = _getFoodCategory();
+  var myLength = categoryList == null ? 0 : categoryList.length;
   return new Flexible(
     child: new ListView.builder(
-      itemCount: categoryList.length,
+      itemCount: myLength,
       itemBuilder: (context, index){
         return new Card(
           child: new ListTile(
-            leading: new Icon(categoryList[index].image, size: 50.0,),
-            title: new Text(categoryList[index].name),
-            subtitle: new Text(categoryList[index].descript),
+            leading: new Image.network('https://flutter.io/images/flutter-mark-square-100.png'),
+            title: new Text(categoryList[index]['name']),
+            subtitle: new Text(categoryList[index]['desc']),
             isThreeLine: true,
             onLongPress: null,
             onTap: (){
               Navigator.of(context).push(new PageRouteBuilder(
                 opaque: true,
                 pageBuilder: (BuildContext context, _, __){
-                  return new MenuPage(categoryList[index].id);
+                  return new MenuPage(categoryList[index]['id']);
                 },
                 transitionsBuilder: (_, Animation<double> animation, __, Widget chile){
                   return new FadeTransition(
@@ -64,26 +87,11 @@ Widget getListCategory(BuildContext context){
     ),
   );
 }
-
-class FoodCategory {
-  final num id;
-  final String name;
-
-  final String descript;
-
-  final IconData image;
-
-  FoodCategory(this.id, this.name, this.descript, this.image);
+//获取请求得到的字符串
+Future<List> getCategory() async {
+  http.Response data = await http.get('http://10.0.2.2:8080');
+  if(data.statusCode == 200)
+    return jsonDecode(data.body);
+  return [];
 }
 
-_getFoodCategory(){
-  List<FoodCategory> foodcategory = [
-    new FoodCategory(1, '甜品', 'this is Descript about the food', Icons.https),
-    new FoodCategory(2, '小吃', 'this is Descript about the food', Icons.book),
-    new FoodCategory(3, '西餐', 'this is Descript about the food', Icons.tablet),
-    new FoodCategory(4, '中餐', 'this is Descript about the food', Icons.child_care),
-    new FoodCategory(5,'水果', 'this is Descript about the food', Icons.favorite),
-    new FoodCategory(6,'汤', 'this is Descript about the food', Icons.favorite),
-  ];
-  return foodcategory;
-}
